17899f7116f6f0ddcfe0b54860f5dca5efce8287
Apple <apple@Apples-MacBook-Pro-59.local>
2019-07-22 15:02:08 -0400

    initialized git

---
+++ Contents.swift
@@ -0,0 +1,42 @@
+/* String: anything between two comments
+ integer: all numbers except decimels
+import Cocoa
+*/
+
+var str = "Hello, playground"//
+//string
+//integer
+//double
+var address = "1313 mockingbird lane"// create or declare the varible adress
+address = "245 Ocean Avenue"// this is changing the address to a new one
+let name = "roya"// let is contant, uses less memory and make a machine run faster , good for perminant
+var empty : Double //data types always in capital, = is assigning a value : is saying idk what it is yet
+var printMe = "hello world"// camel case, more than one word second and third are capitalized
+
+
+let roya = "5+5="
+let ans = 5+5
+print (roya, ans)
+
+let one = "9.7*19=" // you can rename the variable so that it equal to float so it can round the number
+let two = 9.7*19
+print (one, two)
+
+
+var temp = 87
+
+if temp == 87 {
+    print ("i guessed correct")
+}
+else {
+    print("wrong")
+}
+
+var favoriteFood = "ice cream"
+if favoriteFood == "chipotle" {
+    print ("you must like chipotle")
+}
+else if favoriteFood == ("starbucks") {
+    print ("you like starbucks")
+}
+else 
